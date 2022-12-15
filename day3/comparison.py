100e5220ca0bf34816f8dab9a874ddcbe95ee5b5
Lukman Buhari <lukmanbuhari07@gmail.com>
2022-12-15 15:22:09 +0100

    comparison methods

---
+++ day3/comparison.py
@@ -0,0 +1,49 @@
+'''
+base = int(input("Enter the base of the triangle: "))
+height = int(input("Enter the height of the triangle: "))
+area = 1/2 * base * height
+print("The area of the triangle is ", area)
+
+a = int(input("Enter side a: "))
+b = int(input("Enter side b: "))
+c = int(input("Enter side c: "))
+perimeter = a + b + c
+print("The perimeter of the triangke is ", perimeter)
+
+length = int(input("Enter the length of the rectangle: "))
+breadth = int(input("Enter the breadth of the rectangle: "))
+area = length * breadth
+print("The area of the rectangle is ", area)
+
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+slope = (y2 - y1) / (x2 - x1)
+print("The slope is", slope)
+
+print(len("python") > len("dragon"))
+print(("th" in "python") and ("on" in "dragon"))
+
+note = "I hope this course is not full of jargon"
+print("jargon" in note)
+
+note = "python"
+print(str(len(note)))
+
+num = 7 // 3 == int(2.3)
+print(num)
+
+print("10" == 10)
+
+print(int(9.8) == 10)
+
+year = int(input("Enter number of years: "))
+seconds = year * 3600
+print("You have lived for ", seconds, "seconds")
+'''
+print(1, 1 , 1 , 1 , 1, sep=" ")
+print(2, 1 , 2 , 4 , 8, sep=" ")
+print(3, 1 , 3 , 9 , 27, sep=" ")
+print(4, 1 , 4 , 16 , 64, sep=" ")
+print(5, 1 , 5 , 25 , 125, sep=" ")
