adaae0d3e9632fd73d32db065400e26b2389ee4b
Lukman Buhari <lukmanbuhari07@gmail.com>
2022-12-15 13:07:01 +0100

    Variables and forms

---
+++ day2/variables.py
@@ -0,0 +1,46 @@
+# Exercise 1
+print("Day 2: 30 Days of python programming")
+first_name = "Lukman"
+last_name = "Buhari"
+full_name = "Lukman Buhari"
+country = "Nigeria"
+city = "Ibadan"
+age = 23
+year = 1999
+is_married = True
+is_true = False
+is_light_on = True
+first_name, last_name, country, city = "Lukman", "Buhari", "Nigeria", "Ibadan"
+
+
+# Exercise 2
+print((type(first_name)))
+print((type(last_name)))
+print((type(full_name)))
+print((type(country)))
+print((type(city)))
+print((type(age)))
+print((type(year)))
+print((type(is_married)))
+print((type(is_true)))
+print((type(is_light_on)))
+print((len(first_name)))
+print((len(last_name)))
+
+num_one = 5
+num_two = 4
+total = print(num_one + num_two)
+difference = print(num_one - num_two)
+product = print(num_one * num_two)
+division = print(num_one / num_two)
+remainder = print(num_one % num_two)
+exp = print(num_one ** num_two)
+floor_division = print(num_one // num_two)
+
+radius = 30
+area_of_circle = print(3.142 * (radius)**2)
+circum_of_circle = print(2 * 3.142 * radius)
+
+rad = int(input("Enter a radius: "))
+area = 3.142 * (rad ** 2)
+print(area)
