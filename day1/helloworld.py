3848fbd5b662eb5cf4e87685a0724e4814851136
Lukman Buhari <lukmanbuhari07@gmail.com>
2022-12-15 12:24:24 +0100

    Added operations

---
+++ day1/helloworld.py
@@ -0,0 +1,10 @@
+print(3 + 4) #addition
+print(4 - 3) #subtraction
+print(4 % 3) #modulus
+print(4 / 3) #division
+print(3 ** 4) #exponential
+print(4 // 3) #floor division operator
+
+#Euclidean distance
+#between (2,3) and (10,8)
+print(((10-2)**2 + (8 - 3)**2)**0.5)
